a081f6a7daadc126dfe1e112b2de5d0a04b0eb5c
Ziad003 <ziad.hossain15@gmail.com>
2026-09-07 20:48:18 +0600

    gethttpRequestandSenthttpResponseIntheBrowser

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,25 @@
 import { createServer, IncomingMessage, Server } from "node:http";
 
-const server:Server=createServer((req:IncomingMessage,res)=>{
-    console.log(req)
-})
+const server: Server = createServer((req: IncomingMessage, res) => {
+  // console.log(req.url)
+  // console.log(req.method)
 
-server.listen(5000,()=>{
-    console.log("Server is running on the port 5000");
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/" && method === "GET") {
+    // console.log("This is Root route")
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(JSON.stringify({message:"This is root rout"}));
+  }else if(url?.startsWith('/products')){
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(JSON.stringify({message:"This is product rout"}));
+  } else {
+    res.writeHead(404, { "Content-type": "text/plain" });
+    res.end("Route not found");
+  }
+});
+
+server.listen(5000, () => {
+  console.log("Server is running on the port 5000");
 });
