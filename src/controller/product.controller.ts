cfdd72aa571cad5e142f97cb74e06e376fcd01d4
Ziad003 <ziad.hossain15@gmail.com>
2026-09-09 11:35:04 +0600

    Reading files with fs module

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,14 +1,16 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
+import { readProduct } from "../service/product.service";
 
 export const productController=(req:IncomingMessage,res:ServerResponse)=>{
     const url = req.url;
     const method = req.method;
     if(url==="/products" && method==="GET"){
-        const products={
-            id:1,
-            name:"Product-1"
-        }
+        // const products=[{
+        //     id:1,
+        //     name:"Product-1"
+        // }]
 
+        const products=readProduct();
         res.writeHead(200, { "Content-type": "application/json" });
         res.end(JSON.stringify({message:"Product retrive successfully",
             data:products
