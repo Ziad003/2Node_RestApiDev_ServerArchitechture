4f4a4aad00745b7ede8a5d4c1901f00323c14a9e
Ziad003 <ziad.hossain15@gmail.com>
2026-09-09 12:51:14 +0600

    Get Single product

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,9 +1,13 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { readProduct } from "../service/product.service";
+import type { IProduct } from "../types/product.type";
 
 export const productController=(req:IncomingMessage,res:ServerResponse)=>{
     const url = req.url;
     const method = req.method;
+    const urlParts=url?.split('/')
+    const id=urlParts && urlParts[1]==="products" ? Number(urlParts[2]): null;
+    console.log("This is the actual id:", id)
     if(url==="/products" && method==="GET"){
         // const products=[{
         //     id:1,
@@ -12,9 +16,18 @@ export const productController=(req:IncomingMessage,res:ServerResponse)=>{
 
         const products=readProduct();
         res.writeHead(200, { "Content-type": "application/json" });
-        res.end(JSON.stringify({message:"Product retrive successfully",
+        res.end(JSON.stringify({message:"Products retrive successfully",
             data:products
         }));
 
+    }else if(method==="GET" && id !== null){
+        const products=readProduct();
+        const product=products.find((p: IProduct)=>p.id===id)
+        console.log(product)
+
+        res.writeHead(200, { "Content-type": "application/json" });
+        res.end(JSON.stringify({message:"Product retrive successfully",
+            data:product
+        }));
     }
 }
